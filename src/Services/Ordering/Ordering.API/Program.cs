b75060a5500718965c8de76f06858178a5589957
An Le Thien <an.leth@titancorpvn.com>
2024-08-05 09:07:36 +0700

    update infrastructure

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,14 +1,12 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddApplicationService()
     .AddInfrastructureService(builder.Configuration)
     .AddApiService();
-
 var app = builder.Build();
 app.UseApiServices();
 app.Run();
--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -11,7 +11,6 @@ namespace Ordering.Application
     {
         public static IServiceCollection AddApplicationService(this IServiceCollection services)
         {
-
             return services;
         }
     }
